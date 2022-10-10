06c120764985e62ec170a872a0fc07999eff74a5
SShyamKumar <shyamng48@gmail.com>
2022-10-10 20:44:43 +0530

    Notes added

--- ModernJSCWH.js
+++ ModernJSCWH.js
@@ -1037,7 +1037,7 @@ for (let i in fr) {
 
 /////////////////////////////////////
 
-// console.log("Promises starts")
+console.log("Promises starts")
 
 //zomato will notify that order will be delievered in 30 mins, if late
 //also it will be notified
@@ -1047,6 +1047,9 @@ for (let i in fr) {
 //   alert("Hello")
 //   resolve(56)
 // })
+
+// console.log(promise)
+
 
 // console.log("Hello One")
 // setTimeout(function() {
@@ -1062,11 +1065,11 @@ for (let i in fr) {
 // Print downloading 
 // Rest of the script 
 
-// console.log("Promises ends")
+console.log("Promises ends")
 
 ///////////////////////////////////////
 
-console.log("Promise .then() and .catch() starts")
+// console.log("Promise .then() and .catch() starts")
 
 // let p = new Promise((resolve, reject)=>{
 //   console.log("promise is pending")
@@ -1081,37 +1084,83 @@ console.log("Promise .then() and .catch() starts")
 // console.log(p)
 
 
-let p1 = new Promise((resolve, reject)=>{
-  console.log("promise is pending")
-  setTimeout(()=>{
-    // console.log("I am a promise and I am fulfilled")
-    console.log("I am a promise and I am rejected")
-    resolve(true)
-    // reject(new Error("I am an error"))
-  }, 5000)
-})
+// let p1 = new Promise((resolve, reject)=>{
+//   console.log("promise is pending")
+//   setTimeout(()=>{
+//     // console.log("I am a promise and I am fulfilled")
+//     console.log("I am a promise and I am rejected")
+//     resolve(true)
+//     // reject(new Error("I am an error"))
+//   }, 5000)
+// })
 
 
-let p2 = new Promise((resolve, reject)=>{
-  console.log("promise is pending")
-  setTimeout(()=>{
-    // console.log("I am a promise and I am fulfilled")
-    console.log("I am a promise and I am rejected")
-    // resolve(true)
-    reject(new Error("I am an error"))
-  }, 5000)
-})
+// let p2 = new Promise((resolve, reject)=>{
+//   console.log("promise is pending")
+//   setTimeout(()=>{
+//     // console.log("I am a promise and I am fulfilled")
+//     console.log("I am a promise and I am rejected")
+//     // resolve(true)
+//     reject(new Error("I am an error"))
+//   }, 5000)
+// })
 
 // console.log(p1, p2)
 
-p1.then((value)=>{
+// p1.then((value)=>{
+//   console.log(value)
+// })
+
+// p2.catch((error)=>{
+//   console.log(error)
+// })
+
+// console.log("Promise .then() and .catch() ends")
+
+////////////////////////////////////
+
+console.log("Promise API starts")
+
+let p1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+          resolve("Value 1");
+  }, 11000);
+});
+
+let p2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+          // resolve("Value 2");
+          reject(new Error("Error"));
+  }, 2000);
+});
+
+let p3 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+          resolve("Value 3");
+  }, 3000);
+});
+
+// p1.then((value) => {
+//         console.log(value)
+// })
+
+// p2.then((value) => {
+//         console.log(value)
+// })
+
+// p3.then((value) => {
+//         console.log(value)
+// })
+
+// let promise_all = Promise.all([p1, p2, p3])
+// let promise_all = Promise.allSettled([p1, p2, p3])
+// let promise_all = Promise.race([p1, p2, p3])
+// let promise_all = Promise.resolve(6)
+let promise_all = Promise.reject(new Error("Hey"))
+promise_all.then((value) => {
   console.log(value)
 })
 
-p2.catch((error)=>{
-  console.log(error)
-})
+console.log("Promise API ends")
 
-console.log("Promise .then() and .catch() ends")
-
-////////////////////////////////////
+////////////////////////////////////////////////
